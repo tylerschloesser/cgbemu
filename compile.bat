7fde2e66aa62a21ab14834098a8277da5eda61d6
Tyler Schloesser <tylerschloesser@gmail.com>
2012-09-07 11:50:14 -0500

    Changed directory structure and modified compile script

--- compile.bat
+++ compile.bat
@@ -1,3 +1,4 @@
-gcc src/debug.c src/tools.c src/main.c src/cpu.c src/memory.c src/graphics.c -o main.exe -lmingw32 -lSDLmain -lSDL -mwindows -g -static-libgcc
+cd %~dp0src
+gcc debug.c tools.c main.c cpu.c memory.c graphics.c -o ../bin/main.exe -lmingw32 -lSDLmain -lSDL -mwindows -g -static-libgcc
 IF NOT ERRORLEVEL 1 main.exe
 PAUSE
